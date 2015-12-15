c919422e4c87125e871302c39a74a6b56d1e2359
Gabe Abinante <gabinante@clearslide.com>
2015-12-15 09:50:01 -0800

    First Commit / Source add

---
+++ TitleCreator.py
@@ -0,0 +1,24 @@
+import random
+
+Prefixlist = ['Viceroy','Commandant', 'Grand Poo-Bah', 'Archon', 'Duke', 'Chancellor', 'President', 'Marquis', 'Earl', 'Director', 'Chair', 'Head', 'Senior Director']
+
+Prefixlist2 = ['Principal','Chief','Head','Lead', 'Senior', 'Master']
+
+Joblist = ['Solutions', 'Systems', 'Network', 'Security', 'Compliance', 'Information', 'Scalability']
+
+Postfixlist = ['Engineer', 'Architect', 'Designer', 'Consultant', 'Manager', 'Officer']
+
+Postfixlist2 = ['Engineering', 'Management', 'Development', 'Deployment', 'Technical Training', 'Operations', 'Architecture', 'Infrastructure', 'Technology', 'Administration', 'Management', 'Computational Analytics', 'Database Administration']
+
+RandomDecider = random.randint(0, 1)
+RandomPre = random.choice(Prefixlist)
+RandomPre2 = random.choice(Prefixlist2)
+RandomJob = random.choice(Joblist)
+RandomPost = random.choice(Postfixlist)
+RandomPost2 = random.choice(Postfixlist2)
+
+if RandomDecider == 1:
+  print RandomPre + ' of ' + RandomJob + ' ' + RandomPost2
+
+else:
+  print RandomPre2 + ' ' + RandomJob + ' ' + RandomPost
