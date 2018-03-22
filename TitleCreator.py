5580d8d805985b8300dd4ea0d52cf94c5808a933
johan.beisser <beisser@adobe.com>
2018-03-22 09:44:54 -0700

    Enterprisy!

--- TitleCreator.py
+++ TitleCreator.py
@@ -1,26 +1,25 @@
-import random
+from random import seed, randchoice
 
-Prefixlist = ['Viceroy', 'Commandant', 'Grand Poo-Bah', 'Archon', 'Duke', 'Chancellor', 'President', 'Marquis', 'Earl', 'Director', 'Chair', 'Head', 'Senior Director', 'Vice President']
+# make enterprise level
+seed()
 
-Prefixlist2 = ['Principal', 'Chief', 'Head', 'Lead', 'Senior', 'Master']
+def prefix():
+    l = ['Viceroy', 'Commandant', 'Grand Poo-Bah', 'Archon', 'Duke', 'Chancellor', 'President', 'Marquis', 
+            'Earl', 'Director', 'Chair', 'Head', 'Senior Director', 'Vice President', 'Principal', 'Chief', 
+            'Head', 'Lead', 'Senior', 'Master', 'Dark Lord',]
+    return randchoice(l)
 
-Joblist = ['Solutions', 'Systems', 'Network', 'Security', 'Compliance', 'Information', 'Scalability', 'Thought', 'Database', 'Platform', 'Storage', 'Cloud']
+def job():
+    l = ['Solutions', 'Systems', 'Network', 'Security', 'Compliance', 'Information', 'Scalability', 
+            'Thought', 'Database', 'Platform', 'Storage', 'Cloud']
+    return randchoice(l)
 
-Postfixlist = ['Engineer', 'Architect', 'Designer', 'Consultant', 'Manager', 'Officer', 'Leader', 'Janitor']
-
-Postfixlist2 = ['Engineering', 'Management', 'Development', 'Deployment', 'Technical Training', 'Operations', 'Architecture', 'Infrastructure', 'Technology', 'Administration', 'Leadership']
-
-RandomDecider = random.randint(0, 1)
-RandomPre = random.choice(Prefixlist)
-RandomPre2 = random.choice(Prefixlist2)
-RandomJob = random.choice(Joblist)
-RandomPost = random.choice(Postfixlist)
-RandomPost2 = random.choice(Postfixlist2)
+def postfix():
+    l = ['Engineer', 'Architect', 'Designer', 'Consultant', 'Manager', 'Officer', 'Leader', 'Janitor', 'Engineering',
+            'Management', 'Development', 'Deployment', 'Technical Training', 'Operations', 'Architecture', 
+            'Infrastructure', 'Technology', 'Administration', 'Leadership']
+    return randchoice(l)
 
 print("Congratulations! Your new title is:")
 
-if RandomDecider == 1:
-  print(RandomPre + ' of ' + RandomJob + ' ' + RandomPost2)
-
-else:
-  print(RandomPre2 + ' ' + RandomJob + ' ' + RandomPost)
+print(prefix() + ' of ' + job() + ' ' + postfix())
